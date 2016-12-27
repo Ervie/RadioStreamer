7ceb96d16b10f163ea4bfe87c24b136c09de4032
Ervin <bbuchala93@gmail.com>
2016-12-27 08:05:17 +0100

    Minor changes

--- RadioStreamer/RadioStreamer.Domain/Scripts/02_RecommendationProcedure.sql
+++ RadioStreamer/RadioStreamer.Domain/Scripts/02_RecommendationProcedure.sql
@@ -28,11 +28,11 @@ BEGIN
 	
 	/* Get all channels ever listened by user */
 	insert into @ChannelsByDuration (ChannelId, ChannelName, TotalDuration)
-	SELECT chn.Id, chn.Name, SUM(his.Duration) as 'TotalDuration'
+	SELECT chn.Id, chn.Name, SUM(his.Value) as 'TotalDuration'
 	FROM [dbo].History his, [dbo].Channel chn
 	WHERE his.UserId = @UserId AND his.ChannelId = chn.Id
 	GROUP BY chn.Id, chn.Name
-	ORDER BY SUM(his.Duration) desc
+	ORDER BY SUM(his.Value) desc
 	
 	/* Load all tags into temporary table */
 	
