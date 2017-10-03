4f63b835d39a7225c45094cf4242dd8d842a59fc
Bartłomiej Buchała <bbuchala93@gmail.com>
2017-10-03 22:32:35 +0200

    Added initial folders for API and SPA. Removal of unused projects.

---
+++ RadioStreamer/Api/RadioStreamer.API/Controllers/ChannelsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RadioStreamer.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChannelController : Controller
+    {
+        // GET api/channels
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            // temporary hardcoded streams
+
+            return new string[] { "http://radio.vgmradio.com:8040/stream",
+                                     "http://195.150.20.4:8000/rmf_classic",
+                                     "http://redir.atmcdn.pl/sc/o2/Eurozet/live/antyradio.livx?audio=5" };
+        }
+
+        // GET api/channels/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/channels
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/channels/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/channels/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
